71f457747cd72d2b367e759584b650490dfa57cd
mutoairi <146906672+mutoairi@users.noreply.github.com>
2024-12-16 11:16:14 +0900

    Revert "Boxgimic"

--- DirectXGame/AABB.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include"math/Vector3.h"
-
-struct AABB {
-	KamataEngine::Vector3 min;
-	KamataEngine::Vector3 max;
-
-	static bool IsCollision(const AABB& aabb1, const AABB& aabb2);
-};
