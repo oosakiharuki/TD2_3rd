d3cb41de84075f7a9cbd4386fc62bc0003189297
mutoairi <146906672+mutoairi@users.noreply.github.com>
2025-01-09 14:38:01 +0900

    プレイヤーと当たり判定をつなげて確認

--- DirectXGame/Electricity.cpp
+++ DirectXGame/Electricity.cpp
@@ -17,7 +17,50 @@ void Electricity::Initialize(KamataEngine::Model* model, KamataEngine::Model* mo
 }
 
 void Electricity::Update() {
+	if (Flag) {
+		worldTransform3_.translation_.x -= 1.0f;
+		worldTransform3_.scale_.x -= 1.0f;
+	}
 
+	if (Flag2) {
+		worldTransform3_.translation_.x += 1.0f;
+		worldTransform3_.scale_.x -= 1.0f;
+	}
+	worldTransform_.UpdateMatrix();
+	worldTransform2_.UpdateMatrix();
+	worldTransform3_.UpdateMatrix();
+	worldTransform4_.UpdateMatrix();
 }
 
 void Electricity::Draw() {}
+//左用
+
+void Electricity::OnCollision() {
+	Flag = true;
+}
+
+AABB Electricity::GetAABB() { 
+	KamataEngine::Vector3 worldPos = worldTransform_.translation_;
+	AABB aabb;
+
+	aabb.min = {worldPos.x - kWidth / 2.0f, worldPos.y - kHeight / 2.0f, worldPos.z - kWidth / 2.0f};
+	aabb.max = {worldPos.x + kWidth / 2.0f, worldPos.y + kHeight / 2.0f, worldPos.z + kWidth / 2.0f};
+
+	return aabb;
+}
+
+//右側
+
+void Electricity::OnCollision2() {
+	Flag2 = true;
+}
+
+AABB Electricity::GetAABB2() {
+	KamataEngine::Vector3 worldPos = worldTransform2_.translation_;
+	AABB aabb;
+
+	aabb.min = {worldPos.x - kWidth / 2.0f, worldPos.y - kHeight / 2.0f, worldPos.z - kWidth / 2.0f};
+	aabb.max = {worldPos.x + kWidth / 2.0f, worldPos.y + kHeight / 2.0f, worldPos.z + kWidth / 2.0f};
+
+	return aabb;
+}
--- DirectXGame/Electricity.h
+++ DirectXGame/Electricity.h
@@ -3,6 +3,7 @@
 #include <3d/Model.h>
 #include <3d/ObjectColor.h>
 #include <3d/WorldTransform.h>
+#include"AABB.h"
 
 class Electricity {
 public:
@@ -11,6 +12,14 @@ public:
 	void Update();
 
 	void Draw();
+
+	void OnCollision();
+	// AABBを取得
+	AABB GetAABB();
+
+	void OnCollision2();
+	// AABBを取得
+	AABB GetAABB2();
 
 private:
 	KamataEngine::Camera* viewProjection_ = nullptr;
@@ -25,4 +34,8 @@ private:
 	KamataEngine::WorldTransform worldTransform4_;
 	KamataEngine::ObjectColor objColor = {};
 	bool Flag;
+	bool Flag2;
+	// キャラクターの当たり判定サイズ
+	static inline const float kWidth = 1.0f;
+	static inline const float kHeight = 1.0f;
 };
